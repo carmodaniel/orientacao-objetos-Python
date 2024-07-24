4834f476179449e634a45d939cdec2f5b97d91d7
Daniel Carmo <daniel.carmo@softplan.com.br>
2024-07-24 19:27:32 -0300

    inclusão dos métodos Get/Set para  método senha  `@property = GET`  `@senha.setter = SET`

--- ContasBancos.py
+++ ContasBancos.py
@@ -26,8 +26,8 @@ class ContaCorrente:
         return horario_br.strftime('%d/%m/%Y %H:%M:%S') # utilzando strftime para formatar data e hora em dia/mes/Ano Hora:Minuto:Segundo
 
     def __init__(self, nome, cpf, agencia, num_conta):  # O método __init__ é chamado quando um objeto é criado. Ele inicializa os atributos da instância.
-        self._nome = nome
-        self._cpf = cpf
+        self.nome = nome
+        self.cpf = cpf
         self._saldo = 0
         self._limite = None
         self._agencia = agencia
@@ -87,8 +87,20 @@ class CartaoCredito:
         self.validade ='{}/{}'.format(CartaoCredito._data_hora().month, CartaoCredito._data_hora().year+4)
         self.cod_seguranca = '{}{}{}'.format(randint(0,9), randint(0,9), randint(0,9))
         self.limite = 1000
+        self._senha = '1234'
         self.conta_corrente = conta_corrente
         conta_corrente.cartoes.append(self)
+
+    @property
+    def senha(self):
+        return self._senha
+
+    @senha.setter
+    def senha(self, valor):
+        if len(valor) ==4 and valor.isnumeric():
+            self._senha = valor
+        else:
+            print("Nova senha errada")
 
 
 #  programa
@@ -96,11 +108,17 @@ conta = ContaCorrente("Daniel", "111.111.111-00", 123456, 34062)
 
 cartao = CartaoCredito("Daniel", conta)
 
-print(cartao.conta_corrente._num_conta)
+conta.nome = 'Daniel Carmo'
+print(conta.nome)
 
-print(cartao.numero)
-print(cartao.cod_seguranca)
-print(cartao.validade)
+cartao.senha = '1222'
+print(cartao.senha)
+
+#print(cartao.conta_corrente._num_conta)
+
+#print(cartao.numero)
+#print(cartao.cod_seguranca)
+#print(cartao.validade)
 
 # depositando dinheiro
 #conta.deposita(10000)  # utilizando metodo depositar
