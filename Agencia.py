ecebce0b26e1c34cda1638e8a4071ded7a26e008
Daniel Carmo <daniel.carmo@softplan.com.br>
2024-08-05 19:41:58 -0300

    atualização das subclasses

--- Agencia.py
+++ Agencia.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Agencia:
 
     def __init__(self, telefone, cnpj, numero):
@@ -25,30 +28,35 @@ class Agencia:
 
 
 # Criando Subclasses da classe Agencia
-class AgenciaCcomun(Agencia):
-    pass
+class AgenciaComun(Agencia):
+    def __init__(self, telefone, cnpj):
+        super().__init__(telefone,cnpj, numero=randint(1001, 9999))
+        self.caixa = 1000000
 
 
 class AgenciaVirtual(Agencia):
-    pass
 
+    def __init__(self, site, telefone, cnpj): #ciando __init  # criando __init__ da sublclasse
+        self.site = site
+        super().__init__(telefone, cnpj, 1000) # chamado __init__ da classe principal
+        self.caixa = 1000000
 
 class AgenciaPremium(Agencia):
-    pass
-
+    def __init__(self, telefone, cnpj):
+        super().__init__(telefone, cnpj, numero=randint(1001, 9999))
+        self.caixa = 10000000
 
 
 agencia_primeira = Agencia(123456789, 122222122212221, 11111)
 
-agencia_primeira.caixa = 1000000
+agencia_virtual = AgenciaVirtual('www.agencia.virtual.com.br', 222222, 1511111)
+agencia_virtual.verificar_caixa()
+print(agencia_virtual.site)
 
-agencia_primeira.verificar_caixa()
+agencia_comum = AgenciaComun(222222221, 25555566666)
+agencia_comum.verificar_caixa()
 
-agencia_primeira.emprestar_dinheiro(1500, 123456666444, 0.2)
-print(agencia_primeira.emprestimos)
-
-
-agencia_primeira.adicionar_cliente('Daniel', 11111111, 100000)
-print((agencia_primeira.clientes))
+agenciacia_premium = AgenciaPremium(33333333555, 66666666555)
+agenciacia_premium.verificar_caixa()
 
 
